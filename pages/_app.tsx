c92c6fa277e9e23154e71d6b56c5b42e0367b0a2
TaichiShigematsu <shigetaichi1one@gmail.com>
2020-11-08 22:37:42 +0900

    google analytics settings challenge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag';
+import React, {useEffect} from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!gtag.existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      gtag.pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
 
