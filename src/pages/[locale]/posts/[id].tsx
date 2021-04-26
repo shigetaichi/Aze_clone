23e0b45df9677b1e489437bcbba1a69101509039
TaichiShigematsu <shigetaichi1one@gmail.com>
2021-04-26 14:16:58 +0900

    fix: PostTemplateのpropsを調整

--- src/pages/[locale]/posts/[id].tsx
+++ src/pages/[locale]/posts/[id].tsx
@@ -1,4 +1,4 @@
-import { wpBaseUrl, wpGenerateNextAndPrevArray } from 'lib/post';
+import { wpBaseUrl } from 'lib/post';
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import PostTemplate from "components/template/Post/PostTemplate";
 import { fetchWithCache } from "lib/helpers";
@@ -7,27 +7,22 @@ import Head from "next/head";
 // postの中のcssはglobal.cssに記載
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-  let postData, nextAndPrev;
-  const localeParam: string = String(context.query.locale);
+  let postData;
   const idParam: number = Number(context.query.id);
   await Promise.all([
     (async () => {
       postData = await fetchWithCache(`${wpBaseUrl}/wp-json/wp/v2/posts/${idParam}?_fields=id,acf,title,date,modified,content,meta,categories,category_name,tags,translate_group`);
-    })(),
-    (async () => {
-      nextAndPrev = await wpGenerateNextAndPrevArray(`${localeParam}`, idParam);
     })(),
   ]);
   
   return {
     props: {
       postData,
-      nextAndPrev,
     }
   }
 }
 
-const Post = ({postData, nextAndPrev}) => (
+const Post = ({postData}) => (
   <>
     <Head>
       <meta property="og:image" content={postData.acf.eyecatch} key={"og_image"}/>
@@ -36,7 +31,7 @@ const Post = ({postData, nextAndPrev}) => (
       {/*<meta name="keywords" content={postData}/>*/}
       <title>{postData.title.rendered}</title>
     </Head>
-    <PostTemplate postData={postData} nextAndPrev={nextAndPrev}/>
+    <PostTemplate postData={postData}/>
   </>
 );
 
