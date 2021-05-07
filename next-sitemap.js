a4628964600da423a364cacc3db5d03fb04e3b63
TaichiShigematsu <shigetaichi1one@gmail.com>
2021-05-07 13:53:33 +0900

    feat: sitemap生成

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,4 +2,10 @@ module.exports = {
     siteUrl: 'https://azerbaijapan.xyz',
     generateRobotsTxt: true,
     sitemapSize: 7000,
+    exclude: ['/server-sitemap.xml'], // <= exclude here
+    robotsTxtOptions: {
+        additionalSitemaps: [
+            'https://azerbaijapan.xyz/server-sitemap.xml', // <==== Add here
+        ],
+    },
 }
---
+++ src/pages/server-sitemap.xml/index.tsx
@@ -0,0 +1,73 @@
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { fetchWithCache } from "lib/helpers";
+import { ISitemapFiled } from "next-sitemap/dist/@types/interface";
+import { LocaleType } from "context/localeContext";
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  // Method to source urls from cms
+  
+  let fields = [];
+  const supportLocale: LocaleType[] = ['ja', 'az', 'en'];
+  const domain: string = 'https://azerbaijapan.xyz';
+  const apiRoute: string = 'https://azerbaijapan.taichi-sigma2.com/wp-json/wp/v2/posts';
+  
+  // utility functions to generate sitemap
+  const localeFormatter = (data: string): LocaleType => {
+    if (data) {
+      switch (data) {
+        case "ja":
+          return "ja";
+        case "az":
+          return "az";
+        case "en_US":
+          return "en";
+        case "ru":
+          return "ru";
+      }
+    } else {
+      return "ja"
+    }
+  }
+  
+  const generateSitemapObject = (v): ISitemapFiled => ({
+    loc: `${domain}/${localeFormatter(v.meta['_locale'])}/posts/${v.id}`,
+    lastmod: new Date(v.modified).toISOString(),
+    changefreq: 'weekly',
+  })
+  
+  const fetchAndReturnObject = async (url: string) => {
+    const data: Array<any> = await fetchWithCache(url);
+    return data['code'] === "rest_post_invalid_page_number" ? [] : data.map(v => generateSitemapObject(v));
+  }
+  
+  const getAllObject = async (locale: LocaleType) => {
+    for (let i: number = 1; i < 10; i++) {
+      const arr: Array<any> = await fetchAndReturnObject(`${apiRoute}?lang=${locale}&page=${i}`);
+      if (arr.length === 0) {
+        break
+      }
+      fields = fields.concat(arr)
+    }
+  }
+  
+  
+  // set data to fields Array
+  fields = fields.concat(supportLocale.map((lo: LocaleType) => (
+    {
+      loc: `${domain}/${lo}`,
+      lastmod: new Date().toISOString(),
+      changefreq: 'daily',
+    }
+  )));
+  for (const lo of supportLocale) {
+    await getAllObject(lo);
+  }
+  
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+const Sitemap = () => {
+}
+export default Sitemap
